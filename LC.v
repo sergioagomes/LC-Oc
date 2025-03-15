d17f9e5b1579c0b1428428780c51adcd61294889
sergio <thesergio.gomes@gmail.com>
2025-03-15 17:49:34 -0300

    init

---
+++ LC.v
@@ -0,0 +1,15 @@
+Require Import Nat.
+
+(*Lambda Calculus*)
+Inductive term : Type :=
+  | Var : nat -> term
+  | Abs : term -> term
+  | App : term -> term -> term.
+
+Definition id_term : term :=  Abs(Var 0).
+
+Lemma id_term_is_term : exists t : term, True.
+Proof.
+  exists id_term.
+  reflexivity. (* Proves the goal by reflexivity *)
+Qed.
